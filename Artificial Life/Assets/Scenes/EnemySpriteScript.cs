b8768efefb09fa8ebb95303adcb4697f5bc11608
simon <simonxleek@gmail.com>
2018-09-29 18:53:53 -0700

    flicker effect on sprite. My code is all rubbish

---
+++ Artificial Life/Assets/Scenes/EnemySpriteScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpriteScript : MonoBehaviour {
+
+    public float fadeSpeed = 1f;
+    public float fadeTime = 1f;
+    public bool fadeIn = true;
+    public float fade;
+    public SpriteRenderer text;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (fadeIn)
+        {
+            float Fade = Mathf.SmoothDamp(0f, 1f, ref fadeSpeed, fadeTime);
+            text.color = new Color(1f, 1f, 1f, Fade);
+        }
+        if(!fadeIn)
+        {
+            float Fade = Mathf.SmoothDamp(1, 0f, ref fadeSpeed, fadeTime);
+            text.color = new Color(1f, 1f, 1f, Fade);
+        }
+	}
+}
---
+++ Artificial Life/Assets/Scenes/FlickerWhenDmg.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class FlickerWhenDmg : MonoBehaviour {
+    public float spriteBlinkingTimer = 0.0f;
+    public float spriteBlinkingMiniDuration = 0.01f;
+    public float spriteBlinkingTotalTimer = 0.0f;
+    public float spriteBlinkingTotalDuration = 1.0f;
+    public bool startBlinking = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //StartBlinkingEffect();
+        }
+	}
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+       // if()
+        {
+            startBlinking = true;
+        }
+    }
+    private void SpriteBlinkingEffect()
+    {
+        spriteBlinkingTotalTimer += Time.deltaTime;
+        if(spriteBlinkingTotalTimer >= spriteBlinkingTotalDuration)
+        {
+            startBlinking = false;
+            spriteBlinkingTotalTimer = 0.0f;
+            this.gameObject.GetComponent<SpriteRenderer>().enabled = true;//according to sprite
+            return;
+        }
+        spriteBlinkingTimer += Time.deltaTime;
+        if(spriteBlinkingTimer >= spriteBlinkingMiniDuration)
+        {
+            spriteBlinkingTimer = 0.0f;
+            if (this.gameObject.GetComponent<SpriteRenderer>().enabled == true)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().enabled = false;//changes according to our game
+            }
+            else
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().enabled = true;//changes according to our game
+            }
+        }
+    }
+}
